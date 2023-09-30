07ecd0be14c5b1ece9744d05d570643d1d47a6ac
Jim <jime1937@gmail.com>
2023-09-30 13:14:19 -0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header"
 import Meme from "./components/Meme"
+import Footer from "./components/Footer"
 import './App.css'
 
 function App() {
 
   return (
-    <>
+    <div className="app">
       <Header />
       <Meme/>
-    </>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+    return (
+        <footer id="footer">
+           
+            <h6 className="header--footer">Made by JDLG - The images do not belong to me.</h6>
+        </footer>
+    )
+}
